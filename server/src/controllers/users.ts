f50187b76706c4633c78c8a4df9e87cbb53a7215
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-02-23 20:26:29 +0100

    feat: implement backend auth

---
+++ server/src/controllers/users.ts
@@ -0,0 +1,73 @@
+import { NextFunction, Request, Response } from "express";
+import UserModel from "../models/user";
+import { UserDocument } from "../types/user.interface";
+import { Error } from "mongoose";
+import jwt from "jsonwebtoken";
+import { secret } from "../config";
+import { ExpressRequestInterface } from "../types/expressRequest.interface";
+
+const normalizeUser = (user: UserDocument) => {
+  const {email, username, id} = user;
+  const token = jwt.sign({id, email}, secret)
+  return { email, username, id, token}
+}
+
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, username, password } = req.body
+    const newUser = new UserModel({
+      email, username, password
+    });
+    const savedUser = await newUser.save();
+    res.send(normalizeUser(savedUser));
+  } catch (err) {
+    if (err instanceof Error.ValidationError) {
+      const messages = Object.values(err.errors).map(err => err.message);
+      return res.status(422).json(messages)
+    }
+    next(err);
+  }
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email }).select('+password');
+    const errors = {
+      emailOrPassword: 'Incorrect email or password'
+    }
+    if (!user) {
+      return res.status(422).json(errors)
+    }
+
+    const isTheSamePassword = await user.validatePassword(password);
+
+    if (!isTheSamePassword) {
+      return res.status(422).json(errors)
+    }
+
+    res.send(normalizeUser(user))
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+export const currentUser = async(
+  req: ExpressRequestInterface,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user) {
+    return res.status(401);
+  }
+  res.send(normalizeUser(req.user))
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,36 @@
-console.log('server');
+import express from "express";
+import { createServer } from "http";
+import{ Server } from "socket.io";
+import mongoose from "mongoose";
+import * as usersController from "./controllers/users";
+import bodyParser from "body-parser";
+import authMiddleware from "./middlewares/auth";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.get('/' , (req, res) => {
+  res.send("Api is UP");
+});
+
+app.post('/api/users/register', usersController.register);
+app.post('/api/users/login', usersController.login);
+app.get('/api/user', authMiddleware, usersController.currentUser);
+
+io.on('connection', () => {
+  console.log('conncted');
+})
+
+mongoose.connect('mongodb://localhost:27017/trelloClone').then(() => {
+  console.log('connected');
+
+  httpServer.listen(4001, () => {
+    console.log("Api is listening on port 4000");
+  });
+})
+
