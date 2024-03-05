5e76a39dc84c0de5ccda8a52b497ddbe649dcf88
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-03-05 21:30:34 +0100

    feat: add auth guard

---
+++ client/src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthApiService } from '../services/auth-api.service';
+import { inject } from '@angular/core';
+import { map } from 'rxjs';
+
+export const AuthGuard: CanActivateFn = () => {
+  const authService = inject(AuthApiService);
+  const router = inject(Router);
+
+  return authService.isLoggedIn$.pipe(
+    map((isLoggedIn) => {
+      if (isLoggedIn) {
+        return true;
+      }
+
+      router.navigateByUrl('/');
+      return false;
+    })
+  );
+};
